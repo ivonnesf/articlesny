a10393a07b66258d73842392f8b099565b5cc8b5
unknown <you@example.com>
2018-07-12 18:52:18 -0600

    enable environmental variables for heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -27,8 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articlesny";
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articlesny");
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
